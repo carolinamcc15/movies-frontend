7fb008bc83852aa07966238f9bd355e1e14dda7d
Carolina Carranza <00008119@uca.edu.sv>
2023-08-01 15:15:20 -0600

    feat: add function to get menu options

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,25 +1,38 @@
-const API_ENDPOINT_MOVIES = "http://localhost:3010/api/movies";
+import axios from 'axios';
 
-export const handleSubmit = async (formData) => {
-    try {
-        const parsedFormData = {
-            ...formData,
-            duration: parseInt(formData.duration),
-            budget: parseInt(formData.budget),
-        };
+const API_ENDPOINT_MOVIES = 'http://localhost:3010/api/movies';
+const API_ENDPOINT_OPTIONS = 'http://localhost:3010/api/options';
 
-        const response = await fetch(API_ENDPOINT_MOVIES, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(parsedFormData),
-        });
-        const data = await response.json();
-        console.log("Data sent successfully:", data);
-        return data;
-    } catch (error) {
-        console.error("Error sending data:", error);
-        throw error;
-    }
+const handleSubmit = async formData => {
+  try {
+    const parsedFormData = {
+      ...formData,
+      duration: parseInt(formData.duration),
+      budget: parseInt(formData.budget),
+    };
+
+    const response = await axios.post(API_ENDPOINT_MOVIES, parsedFormData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = response.data;
+    console.log('Data sent successfully:', data);
+    return data;
+  } catch (error) {
+    console.error('Error sending data:', error);
+    throw error;
+  }
 };
+
+const getOptions = async () => {
+  try {
+    const response = await axios.get(API_ENDPOINT_OPTIONS);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { handleSubmit, getOptions };
