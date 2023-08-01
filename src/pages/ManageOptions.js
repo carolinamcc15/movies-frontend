57f19cdfc00b6eb052a5e4b7842d22bb24746b61
Carolina Carranza <00008119@uca.edu.sv>
2023-08-01 17:08:21 -0600

    feat: create application pages components

---
+++ src/pages/ManageOptions.js
@@ -0,0 +1,7 @@
+export const ManageOptionsPage = () => {
+  return (
+    <div>
+      <h1>Administrar opciones</h1>
+    </div>
+  );
+};
---
+++ src/pages/MoviesList.js
@@ -0,0 +1,7 @@
+export const MoviesListPage = () => {
+  return (
+    <div>
+      <h1>Listado de películas</h1>
+    </div>
+  );
+};
