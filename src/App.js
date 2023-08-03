cbf27b1ddb27509f21b4225b560580ddde1a7c02
Carolina Carranza <00008119@uca.edu.sv>
2023-08-02 23:19:14 -0600

    style: add global styles and primary color

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { OptionsProvider } from './context/Options';
 import Routes from './components/routes/Routes';
-import './App.css';
+import { ConfigProvider } from 'antd';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: '#0ea5e9',
+            },
+          }}
+        >
         <OptionsProvider>
           <Routes />
         </OptionsProvider>
+        </ConfigProvider >
       </BrowserRouter>
     </div>
   );
