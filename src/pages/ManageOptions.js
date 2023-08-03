117ee6d348537e2d11b782579d5f948eae5425d0
Carolina Carranza <00008119@uca.edu.sv>
2023-08-03 02:33:43 -0600

    style: center pages content vertically

--- src/pages/ManageOptions.js
+++ src/pages/ManageOptions.js
@@ -29,21 +29,23 @@ export const ManageOptionsPage = () => {
   };
 
   return (
-    <div className='flex flex-col gap-8 md:p-4'>
-      {contextHolder}
-      <div className='flex flex-col gap-5'>
-        <h1 className='text-2xl font-semibold '>Administrar opciones</h1>
-        <p>En esta sección puedes habilitar o deshabilitar las opciones disponibles en el menú</p>
+    <div className=' h-full flex items-center justify-center'>
+      <div className='flex flex-col gap-8 md:p-4 w-full max-w-[800px] m-auto'>
+        {contextHolder}
+        <div className='flex flex-col gap-5'>
+          <h1 className='text-2xl font-semibold '>Administrar opciones</h1>
+          <p>En esta sección puedes habilitar o deshabilitar las opciones disponibles en el menú</p>
+        </div>
+        <main>
+          <section className='grid grid-cols-1 sm:grid-cols-2 gap-5'>
+            {options
+              .filter(option => option.path !== siteRoutes.MANAGE_OPTIONS)
+              .map(option => (
+                <OptionCard key={option.id} option={option} onToggle={onToggle} />
+              ))}
+          </section>
+        </main>
       </div>
-      <main>
-        <section className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5'>
-          {options
-            .filter(option => option.path !== siteRoutes.MANAGE_OPTIONS)
-            .map(option => (
-              <OptionCard key={option.id} option={option} onToggle={onToggle} />
-            ))}
-        </section>
-      </main>
     </div>
   );
 };
--- src/pages/MoviesForm.js
+++ src/pages/MoviesForm.js
@@ -2,9 +2,11 @@ import MovieForm from '../components/MovieForm';
 
 export const MoviesFormPage = () => {
   return (
-    <div className='flex flex-col gap-8 md:p-4'>
-      <h1 className='text-2xl font-semibold text-center'>Crea una película</h1>
-      <MovieForm />
-    </div>
+    <main className=' h-full flex items-center justify-center'>
+      <div className='flex flex-col gap-8 md:p-4'>
+        <h1 className='text-2xl font-semibold w-full max-w-[650px] m-auto'>Crea una película</h1>
+        <MovieForm />
+      </div>
+    </main>
   );
 };
